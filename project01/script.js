dd9cfe7da91bf9175a674cd1dc59bdb6181e7dd2
Shivam Singh <shivamsingh8215@gmail.com>
2026-06-28 21:08:20 +0530

    Final: Complete clone portfolio website

--- project01/script.js
+++ project01/script.js
@@ -45,7 +45,7 @@ function circleChaptakaro(){
 
     // var xdiff = dets.clientX - xprev;
     // var ydiff = dets.clientY - yprev;
-    
+
     // Keep the scale value between 0.8 and 1.2
     xscale = gsap.utils.clamp(.8, 1.2, dets.clientX - xprev);
     yscale = gsap.utils.clamp(.8, 1.2, dets.clientY - yprev);
@@ -54,23 +54,12 @@ function circleChaptakaro(){
     yprev = dets.clientY;
 
     circleMouseFollower(xscale, yscale);
-    timeout = setTimeout(function () {
-      document.querySelector(
-        "#minicircle"
-      ).style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(1, 1)`;
+    timeout = setTimeout(function(){
+      document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(1, 1)`;
     }, 100);
   });
 
 }
-
-
-
-
-
-
-
-
-
 
 function circleMouseFollower(xscale, yscale) {
   window.addEventListener("mousemove", function (dets) {
@@ -83,3 +72,34 @@ function circleMouseFollower(xscale, yscale) {
 circleChaptakaro();
 circleMouseFollower();
 firstPageAnimation();
+
+
+//teeno element ko select kro, uske baad tino par ek mouse move lagao, jab mousemove ho to ye pata kro ki mouse kaha pr hai, jiska mtlb hai mouse ki x and y position pata kro, ab mouse ki x y position ke badale us image ko show kro and us image ko move kro, move krte waqt rotate kro and jaise jaise mouse tez chale waise waise rotation bhi tez ho jaye
+
+document.querySelectorAll(".elem")
+.forEach(function(elem){
+  var rotate=0;
+  var diffrot=0;
+
+  elem.addEventListener("mouseleave", function(dets){
+    gsap.to(elem.querySelector("img"), {
+      opacity: 0,
+      ease: Power3,
+      duration: 0.5
+    });
+  });
+
+  elem.addEventListener("mousemove", function(dets){
+    var diff=dets.clientY-elem.getBoundingClientRect().top;
+    diffrot=dets.clientX-rotate;
+    rotate=dets.clientX;
+    
+    gsap.to(elem.querySelector("img"), {
+      opacity: 1,
+      ease: Power3,
+      top: diff,
+      left: dets.clientX,
+      rotate: gsap.utils.clamp(-20, 20, diffrot*0.5)
+    });
+  });
+});
